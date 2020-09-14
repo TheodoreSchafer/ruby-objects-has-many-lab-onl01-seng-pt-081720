265314038f77736bf3c6deebe38d4b5811a39568
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-14 15:56:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,13 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :song
   
   def initialize(name)
     @name = name
     @songs = []
   end
+
+  def songs
+    Song.all.select do {|song| song.artist == self}
 
 
 end
